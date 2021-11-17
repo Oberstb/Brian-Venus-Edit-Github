f12d7a4f0f5f9ab306db7f152bc3b2d4a2006a6b
Oberstb <brendan.hayes-oberst@enersys.com>
2021-11-17 17:09:43 -0500

    combined files again
    combined and consolidated

--- BCU_AUX_Venus-combined.py
+++ BCU_AUX_Venus-combined.py
@@ -1,27 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 13:09:44 2021
 
+@author: venusb
+"""
 
-
-
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d 
+import scipy.optimize as spo
 
+
+
+
+cols = [0,1,2] ## 0 is temperatures 1 is max resistance 2 is min resistance 3 is voutlookuptable
+df = pd.read_excel('erroranalysis.xlsx',sheet_name ="BCU_Aux_Copy",usecols=cols, header = 0,index_col=0) #excel data is dict 
+
+
+#Current lookup table will be used as initial guess for optimization
+x0 = [-40, 0, 20, 70, 110, 150, 4.9688630104064941, 4.6877126693725586, 4.2523789405822754, 2.2073507308959961, 0.9325964450836182, 0.3815633654594421 ]
+
+
+global Rup, Vsupply, tol
+Rup = 2200
+Vsupply = 5
+
+tol = 1.01 #1% tolerance on voltage supply rail and pull up resistor
+
+#Determine the min and max expected voltage given tolerances of pull up and supply voltage
+minVmeas = Vsupply/tol*(df['NTCminR']/(df['NTCminR']+Rup*tol))
+maxVmeas = Vsupply*tol*(df['NTCmaxR']/(df['NTCmaxR']+Rup/tol))
+
+#Create new dataframe of V measurements
+Vmeasdf = minVmeas.to_frame()
+Vmeasdf = Vmeasdf.join(maxVmeas)
+Vmeasdf['Ave'] = Vmeasdf.mean(axis=1)
+
+tstart = -20
+tend = 103
+
+
+
+
+
+#only look at temperatures from -20 to 103C
+
+def cost(x0):
+
+    yinit=x0[:int(len(x0)/2)]
+    xinit=x0[int(len(x0)/2):] #make the x voltage, so that we are comparing temperatures for error
+    
+    #narrow down the dataframe being used for temperatures
+    dfapp = Vmeasdf['Ave'].loc[tstart:tend]
+    
+    lut = interp1d(xinit, yinit, fill_value = "extrapolate") #input voltage output temperature  
+    #Resulting temperature using the lookup tables
+    Vmeasdf.index
+
+    lutTemp = lut(dfapp)
+    #measured - actual error
+    error = lutTemp - dfapp.index
+    
+    #weight the error so that negative errors at hot temperatures, and postive errors at cold temperatures are worse  
+    idx = (error<0) & (dfapp.index.values>55)
+    hoterror = idx * error * 5
+    idx = (error>0) & (dfapp.index.values<0)
+    colderror = idx * error * 5
+    
+    newerror = error + hoterror + colderror
+    
+    #return a single value for RMS error
+    RMS = np.sqrt(np.average(newerror**2))
+    
+    return RMS
 
 
 def initialplotfunction(x0,label1,label2):
 
-    cols = [0,1,2] ## 0 is temperatures 1 is max resistance 2 is min resistance 3 is voutlookuptable
-    df = pd.read_excel('erroranalysis.xlsx',sheet_name ="BCU_Aux_Copy",usecols=cols, header = 0,index_col=0) #excel data is dict 
     
-    #Current lookup table will be used as initial guess for optimization
-    ##x0 = [-40, 0, 20, 70, 110, 150, 4.9688630104064941, 4.6877126693725586, 4.2523789405822754, 2.2073507308959961, 0.9325964450836182, 0.3815633654594421 ]
-    
-    
-    Rup = 2200
-    Vsupply = 5
-    
-    tol = 1.01 #1% tolerance on voltage supply rail and pull up resistor
     
     #Determine the min and max expected voltage given tolerances of pull up and supply voltage
     maxVmeas = (Vsupply*tol)*(df['NTCmaxR']/(df['NTCmaxR']+Rup/tol)) ## correct
@@ -33,8 +90,6 @@ def initialplotfunction(x0,label1,label2):
     Vmeasdf = Vmeasdf.join(maxVmeas)
     Vmeasdf['Ave'] = Vmeasdf.mean(axis=1)
     
-    tstart = -30
-    tend = 105
     yinit=x0[:int(len(x0)/2)]
     xinit=x0[int(len(x0)/2):] #make the x voltage, so that we are comparing temperatures for error
     
@@ -80,3 +135,14 @@ def initialplotfunction(x0,label1,label2):
     plt.xlabel('Actual Temperature (C)')
     plt.ylabel('Temperature Error (lookuptable - Actual)')
     plt.legend(loc='lower right', ncol=1, shadow=True, fancybox=True)
+
+result = spo.minimize(cost,x0, method = 'Nelder-Mead')
+
+
+initialplotfunction(x0, 'initial lookup table', 'Original')
+
+initialplotfunction(result.x,'optimized lookup table','Optimized')
+
+
+
+
